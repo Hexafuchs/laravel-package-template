d63c8b77cf555e6ab1f6c9f68facdb3730454431
Leo <leo@hexafuchs.de>
2024-07-23 10:28:51 +0200

    :technologist: Add convience helpers if the migration should not be called from the stub

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -2,9 +2,10 @@
 
 namespace Hexafuchs\Skeleton\Tests;
 
-use Illuminate\Database\Eloquent\Factories\Factory;
-use Orchestra\Testbench\TestCase as Orchestra;
 use Hexafuchs\Skeleton\PackageServiceProvider;
+use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Filesystem\Filesystem;
+use Orchestra\Testbench\TestCase as Orchestra;
 
 class TestCase extends Orchestra
 {
@@ -28,9 +29,29 @@ class TestCase extends Orchestra
     {
         config()->set('database.default', 'testing');
 
+        // $this->backwardCompatibleDatabase();
+
         /*
         $migration = include __DIR__.'/../database/migrations/create_skeleton_table.php.stub';
         $migration->up();
         */
+    }
+
+    /**
+     * RefreshDatabase, loadMigrationsFrom and In-Memory Database dont work together before this pull request
+     * https://github.com/laravel/framework/pull/47912 was merged. This function replaces the in-memory database with
+     * a local one in the workbench directory.
+     *
+     * @return void
+     */
+    public function backwardCompatibleDatabase()
+    {
+        $db = 'workbench/.testing.db';
+        $filesystem = new Filesystem();
+        config()->set('database.connections.testing.database', $db);
+
+        if ($filesystem->missing($db)) {
+            $filesystem->put($db, "");
+        }
     }
 }
